552a401f0d70af11d052925d8d7b53ca7e7262b0
MerikeR <meriker777@gmail.com>
2024-11-04 15:17:03 +0200

    Hello

--- src/DayOne.java
+++ src/DayOne.java
@@ -7,7 +7,6 @@ public class DayOne {
 
         System.out.println("Merike");
 
-         */
 
         int a = 1;
         int b = 2;
@@ -17,6 +16,19 @@ public class DayOne {
             System.out.println("b is smaller than a");
         } else {
             System.out.println("b is equal to a");
+        }
+         */
+
+        int a = -2;
+        int b = 3;
+        if (b > a && a > 0) {
+            System.out.println("b is bigger than a, and a is positive");
+        } else if (a > b && b > 0) {
+            System.out.println("a is bigger than b, and b is positive");
+        } else if (a == b || (a < 0 && b < 0)) {
+            System.out.println("a and b are either equal or both are negative");
+        } else {
+            System.out.println("No specific conditions were met");
         }
     }
 }
