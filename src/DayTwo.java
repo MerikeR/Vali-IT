68260abd159bd212db9163db3c590424b808c90b
MerikeR <meriker777@gmail.com>
2024-11-05 12:06:46 +0200

    Päev 2 - scanneri ülesanded

--- src/DayTwo.java
+++ src/DayTwo.java
@@ -2,12 +2,49 @@ import java.util.Scanner;
 
 public class DayTwo {
     public static void main(String[] args) {
+
+        // 1. Hello World
+        System.out.println("Hello World!");
+
+         /*
+        // 2. Simple Calculator
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter first number: ");
-        int n = scanner.nextInt();
-        System.out.println(n*n);
-        System.out.println("Enter second number: ");
-        int l = scanner.nextInt();
-        System.out.println(n*l);
+        System.out.print("Enter first number: ");
+        float a = scanner.nextFloat();
+        System.out.print("Enter second number: ");
+        float b = scanner.nextFloat();
+        System.out.println("First number + second number = " + (a+b));
+        System.out.println("First number - second number = " + (a-b));
+        System.out.println("First number * second number = " + (a*b));
+        System.out.println("First number / second number = " + (a/b));
+
+
+        // 3. Odd or Even
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int c = scanner.nextInt();
+        if (c % 2 == 0) {
+            System.out.println("This number is even");
+        } else {
+            System.out.println("This number is odd");
+        }
+        */
+
+        // 4. Greatest Number
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int d = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int e = scanner.nextInt();
+        System.out.print("Enter third number: ");
+        int f = scanner.nextInt();
+        if (d>e && d>f) {
+            System.out.println("Greatest number is " + d);
+        } else if (e>d && e>f) {
+            System.out.println("Greatest number is " + e);
+        } else {
+            System.out.println("Greatest number is " + f);
+        }
+
     }
 }
