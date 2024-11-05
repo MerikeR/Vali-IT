440bedeb2513bc190ab5dfc028ef0510dde594d0
MerikeR <meriker777@gmail.com>
2024-11-05 10:15:42 +0200

    Päev 2 - scanneri importimine

---
+++ src/DayTwo.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class DayTwo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int n = scanner.nextInt();
+        System.out.println(n);
+    }
+}
