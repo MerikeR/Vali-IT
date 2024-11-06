b92a53b466bd629bb6f81129e41902bcde8f3af4
MerikeR <meriker777@gmail.com>
2024-11-06 14:56:08 +0200

    Päev 3 meetodid

--- src/Day3.java
+++ src/Day3.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
+
 public class Day3 {
+
+
+    /*
         public static void main(String[] args) {
         getUser();
     }
@@ -27,4 +32,41 @@ public class Day3 {
     private static void getPassword(int number) {
         System.out.println("...");
     }
+
+
+
+    // Lisame meetodi, mis prindib ekraanile Hello
+    public static void main(String[] args) {
+        getHello();
+    }
+    private static void getHello(){
+        System.out.println("Hello");
+    }
+
+
+    public static void main(String[] args) {
+        getName();
+    }
+    private static void getName() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Your name is " + name);
+        getAge();
+    }
+    public static int getAge() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = input.nextInt();
+        return age;
+    }
+    private static void CheckUserAge(int age) {
+        if(age<18) {
+            System.out.println("You are younger than 18 years old!");
+        } else System.out.println("Your age is " + age);
+    }
+
+     */
+
 }
+
