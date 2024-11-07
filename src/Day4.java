f159f6c3d2135609ce09a5fc6a761a005b6fe77c
MerikeR <meriker777@gmail.com>
2024-11-07 12:54:53 +0200

    Päev 4 array-loop

---
+++ src/Day4.java
@@ -0,0 +1,71 @@
+public class Day4 {
+    public static void main(String[] args) {
+        /*
+        // E1.
+        int[] myArray1 = new int [5];
+        myArray1[0]++;
+        myArray1[1]=myArray1[1]+2;
+        myArray1[2]=myArray1[1]+1;
+        myArray1[3]=myArray1[3]+4;
+        myArray1[4]=5;
+        System.out.println(Arrays.toString(myArray1));
+
+        // E2.
+        int[] myArray2 = {10, 20, 30, 40, 50};
+        System.out.println(myArray2[2]);
+
+        // E3.
+        int[] myArray3 = {5, 10, 15, 20};
+        myArray3[1] = 12;
+        System.out.println(Arrays.toString(myArray3));
+
+        // E4.
+        int[] myArray4 = {8, 6, 7, 5, 3, 0, 9};
+        int length = myArray4.length;
+        System.out.println(length);
+
+        // E5.
+        int[] myArray5 = {4, 2, 8, 1, 6};
+        int sumMyArray5 = myArray5[0]+myArray5[1]+myArray5[2]+myArray5[3]+myArray5[4];
+        System.out.println(sumMyArray5);
+
+
+        // While loop
+        int count = 1;
+        while (count <= 5) {
+            System.out.println("Count is: " + count);
+            count++;
+            System.out.println("Count is after count++: " + count);
+        }
+
+        // Do...while Loop
+        int count1 = 1;
+        do {
+            System.out.println("Count is: " + count1);
+            count1++;
+        }
+        while (count1 <= 5);
+
+        // For Loop
+        for (int count2 = 1; count2 <= 5; count2++) {
+            System.out.println("Count: " + count2);
+        }
+
+        // Foreach Loop
+        String[] fruits = {"Apple", "Banana", "Orange"};
+        for (String fruit : fruits) {
+            System.out.println("Fruit: " + fruit);
+        }
+        */
+
+        // E1.
+        int[] myArray = new int[10];
+        for (int a = 1; a <= myArray.length; a++) {
+            System.out.println(a);
+        }
+
+        // E2.
+
+    }
+}
+
